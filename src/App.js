e8419587f82085dd743fdacebba5f91a9c7a9d67
Avinash-Saini01 <87408580+Avinash-Saini01@users.noreply.github.com>
2024-07-10 15:21:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-// App.js
-
-import React from "react";
-import styled from "styled-components";
-import AddForm from "./component/AddForm";
-import TodoListContainer from "./component/TodoListContainer";
+import React from 'react';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AddForm from './component/AddForm';
+import TodoListContainer from './component/TodoListContainer';
+import TodoDetails from './component/TodoDetails';
 
 const App = () => {
   return (
-      <StContainer>
-        <AddForm />
-        <TodoListContainer />
-      </StContainer>
+      <Router>
+        <StContainer>
+          <Routes>
+            <Route exact path="/" element={
+              <>
+                <AddForm />
+                <TodoListContainer />
+              </>
+            } />
+            <Route path="/details/:id" element={<TodoDetails />} />
+          </Routes>
+        </StContainer>
+      </Router>
   );
 };
 
